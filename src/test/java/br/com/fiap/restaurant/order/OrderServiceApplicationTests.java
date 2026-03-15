90d188afc674583f5d4225f3750157e8c846b9dc
Alexandre Belisário Duarte Leite de Andrade <alex_dev@outlook.com.br>
2026-03-15 09:16:23 -0300

    first commit

---
+++ src/test/java/br/com/fiap/restaurant/order/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.restaurant.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
